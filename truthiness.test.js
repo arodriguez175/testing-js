baff6b21708b4588ae1e153726daa61c4a6edf2c
alex rodriguez <rodriguez.alexander223@gmail.com>
2023-03-02 17:35:24 -0500

    Tested truthiness to practice different matcher equivalents.

---
+++ truthiness.test.js
@@ -0,0 +1,19 @@
+describe("truthiness", () => {
+  test("null", () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+  });
+
+  test("zero", () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy(); // 0 is always returns false unless it's used in math
+    expect(z).toBeFalsy();
+  });
+});
