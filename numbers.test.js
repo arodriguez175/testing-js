95dea57af3fa8b42653a7d916d1f7fe6a0d739a8
alex rodriguez <rodriguez.alexander223@gmail.com>
2023-03-02 18:39:47 -0500

    Added code to test floating point numbers properly to prevent failing test cases due to rounding errors.

--- numbers.test.js
+++ numbers.test.js
@@ -10,4 +10,12 @@ describe("numbers", () => {
     expect(value).toBe(4);
     expect(value).toEqual(4);
   });
+
+  // For floating point equality, use toBeCloseTo,
+  // to avoid rounding errors
+  test("adding floating point numbers", () => {
+    const value = 0.1 + 0.2;
+    // expect(value).toBe(0.3); Won't work because of rounding error
+    expect(value).toBeCloseTo(0.3); // This works
+  });
 });
