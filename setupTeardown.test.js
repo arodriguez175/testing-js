b0f169be3aac2ec70efc53129d97630a35d74aa9
alex rodriguez <rodriguez.alexander223@gmail.com>
2023-03-06 17:42:19 -0500

    Added code to see the scope of before* and after* hooks.

--- setupTeardown.test.js
+++ setupTeardown.test.js
@@ -71,3 +71,34 @@ describe("one time setup", () => {
     expect(isCity("San Juan")).toBeTruthy();
   });
 });
+
+// The before* and after* hooks in a describe block only apply within the scope
+// of that describe block.
+
+// Applies to all tests in this file
+beforeEach(() => {
+  return initializeCityDatabase();
+});
+
+test("city database has Vienna", () => {
+  expect(isCity("Vienna")).toBeTruthy();
+});
+
+test("city database has San Juan", () => {
+  expect(isCity("San Juan")).toBeTruthy();
+});
+
+describe("matching cities to foods", () => {
+  // Applies only to tests in this describe block
+  beforeEach(() => {
+    return initializeFoodDatabase();
+  });
+
+  test("Vienna <3 veal", () => {
+    expect(isValidCityFoodPair("Vienna", "Wiener Schnitzel")).toBe(true);
+  });
+
+  test("San Juan <3 plantains", () => {
+    expect(isValidCityFoodPair("San Juan", "Mofongo")).toBe(true);
+  });
+});
