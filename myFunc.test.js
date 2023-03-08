6c42c01391f6196ff8f0604a995286436de0fdca
alex rodriguez <rodriguez.alexander223@gmail.com>
2023-03-08 10:44:32 -0500

    Added code to test different mock return values.

--- myFunc.test.js
+++ myFunc.test.js
@@ -39,3 +39,13 @@ describe(".mock properties", () => {
     expect(myMock2.mock.contexts[0]).toBe(b);
   });
 });
+
+describe("mock return values", () => {
+  const myMock = jest.fn();
+  console.log(myMock());
+  // undefined
+
+  myMock.mockReturnValueOnce(10).mockReturnValueOnce("x").mockReturnValue(true);
+  console.log(myMock(), myMock(), myMock(), myMock());
+  // 10, x, true, true
+});
