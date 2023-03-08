dc0b776fd88f01ac570505e936b73762be58cde3
alex rodriguez <rodriguez.alexander223@gmail.com>
2023-03-08 11:09:08 -0500

    Added another example to mock return values.

--- myFunc.test.js
+++ myFunc.test.js
@@ -48,4 +48,16 @@ describe("mock return values", () => {
   myMock.mockReturnValueOnce(10).mockReturnValueOnce("x").mockReturnValue(true);
   console.log(myMock(), myMock(), myMock(), myMock());
   // 10, x, true, true
+
+  const filterTestFn = jest.fn();
+
+  // Make the mock return `true` for the first call,
+  // and `false` for the second call
+  filterTestFn.mockReturnValueOnce(true).mockReturnValueOnce(false);
+  const result = [11, 12].filter((num) => filterTestFn(num));
+  console.log(result);
+  // [11]
+
+  console.log(filterTestFn.mock.calls[0][0]); // 11
+  console.log(filterTestFn.mock.calls[1][0]); // 12
 });
